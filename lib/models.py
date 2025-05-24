deff602b101b2c183e62eaad6195198de4750c26
shirocodes <ishirapt@gmail.com>
2025-05-24 16:06:56 +0300

    Initialized project with ORM via an Entity Relationship Diagram.

--- lib/models.py
+++ lib/models.py
@@ -13,8 +13,8 @@ class Company(Base):
     __tablename__ = 'companies'
 
     id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    founding_year = Column(Integer())
+    name = Column(String(), nullable=False)
+    founding_year = Column(Integer(), nullable=False)
 
     def __repr__(self):
         return f'<Company {self.name}>'
@@ -23,7 +23,20 @@ class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name= Column(String(), nullable=False)
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+    
+    id = Column(Integer(), primart_key=True)
+    item_name = Column(String(), nullable=False)
+    value = Column(Integer(), nullable=False)
+    company_id = Column(Integer(), ForeignKey('companies.id'), nullable=False)
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+    
+    def __repr__(self):
+        return f'<Freebie {self.item_name}'
+    
