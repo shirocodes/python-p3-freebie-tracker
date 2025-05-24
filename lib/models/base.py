2669ef63605ec0d858674181cdb755d84d88b58e
shirocodes <ishirapt@gmail.com>
2025-05-24 23:47:56 +0300

    Moved Base, metadata, and naming conventions to separate file.

---
+++ lib/models/base.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData
+
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
