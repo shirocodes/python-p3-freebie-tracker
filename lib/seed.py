4c6440a0b58d023e808f767e3f13938b1d044bfa
shirocodes <ishirapt@gmail.com>
2025-05-24 17:06:37 +0300

    Seeded database with companies, devs, & freebies.

--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,52 @@
 #!/usr/bin/env python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Company, Dev, Freebie
 
-# Script goes here!
+from faker import Faker
+import random
+
+# defining database connection
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# initializing faker
+fake = Faker()
+
+# creating instances
+companies = []
+for _ in range(7):
+    company = Company(
+        name=fake.company(),
+        founding_year=random.randint(2000,2023)
+    )
+    companies.append(company)
+session.add_all(companies)
+session.commit()
+
+devs = []
+for _ in range(7):
+    dev = Dev(
+        name=fake.name()
+    )
+    devs.append(dev)
+session.add_all(devs)
+session.commit()
+
+freebies = []
+item_names = ['Pens', 'Tshirts', 'Uber-discounts', 'Notebook', 'stickers']
+for _ in range(10):
+    freebie = Freebie(
+        item_name=random.choice(item_names),
+        value=random.randint(1,100),
+        company=random.choice(companies),
+        dev=random.choice(devs)
+    )
+    freebies.append(freebie)
+session.add_all(freebies)
+session.commit()
+
+print("successfuly seeded database!")
+
+
