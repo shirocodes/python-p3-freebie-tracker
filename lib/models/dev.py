858ad9ee970e8b1b4ea5caf1d1fa133ebb849771
shirocodes <ishirapt@gmail.com>
2025-05-25 09:55:34 +0300

    Added aggregate methods and refactored Dev with cascades.

--- lib/models/dev.py
+++ lib/models/dev.py
@@ -10,8 +10,31 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String(), nullable=False)
     
-    freebies = relationship('Freebie', backref=backref('dev'))
+    freebies = relationship('Freebie', backref=backref('dev'), cascade='all, delete-orphan')
     companies = relationship('Company', secondary=company_dev, back_populates='devs')
 
     def __repr__(self):
-        return f'<Dev {self.name}>'
+        return (
+            f'Dev(id={self.id},'
+            f'name={self.name})'
+        )
+    
+    def received_one(self, item_name:str) -> bool:
+        """
+        Checks if a dev has a freebie with the indicated item name.
+        """
+        return any(freebie.item_name == item_name for freebie in self.freebies)
+      
+    def give_away(self, dev, freebie):  
+        """
+        if a dev already owns a certain freebie, this method allows the dev
+        to transfer such a freebie to another dev.
+        """
+        if freebie in self.freebies:
+            freebie.dev = dev
+            return freebie
+        else:
+            raise ValueError(f"{self.name} does not own this freebie.")
+            
+    
+    
